d5a8fe9ab983f0ee7370289eb71291ac458fa3dc
Schnitzel <michael@amazeelabs.com>
2014-01-28 14:08:01 +0100

    Update drushrc.php

--- drush-deployment/drushrc.php
+++ drush-deployment/drushrc.php
@@ -1,3 +1,5 @@
 <?php
 $options['package-handler'] = 'git_drupalorg';
 $options['gitsubmodule'] = TRUE;
+$command_specific['sql-sync'] = array('no-cache' => TRUE, 'create-db' => TRUE, 'no-ordered-dump' => TRUE);
+$command_specific['rsync'] = array('verbose' => TRUE);

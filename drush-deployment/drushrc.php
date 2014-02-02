66d53a3a381b6e01b637965f31f1140ecaf4b34f
Bastian Widmer <b.widmer@dasrecht.net>
2014-02-02 19:13:44 +0100

    adding create-db false

--- drush-deployment/drushrc.php
+++ drush-deployment/drushrc.php
@@ -1,5 +1,5 @@
 <?php
 $options['package-handler'] = 'git_drupalorg';
 $options['gitsubmodule'] = TRUE;
-$command_specific['sql-sync'] = array('no-cache' => TRUE, 'no-ordered-dump' => TRUE);
+$command_specific['sql-sync'] = array('no-cache' => TRUE, 'no-ordered-dump' => TRUE, 'create-db' => FALSE);
 $command_specific['rsync'] = array('verbose' => TRUE);
